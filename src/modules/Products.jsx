b79bbf4da52e50f92570c24d1b9b0648d2974c98
Julia Kuznetsova <grace_foto@mail.ru>
2024-08-03 18:51:54 +0300

    change categories in header menu

--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -1,13 +1,25 @@
-import { products } from "../products";
+import { useEffect } from "react";
 import { Product } from "./Product";
+import { useProducts } from "../context/ProductContext";
+import { useSearchParams } from "react-router-dom";
 
-export const Products = () => (
-  <section className="products">
-    <div className="container">
-      <h1 className="products__title">Чай</h1>
-      <ul className="products__list">
-        {products.map((item) => (<Product key={item.id} data={item}/>))}
-      </ul>
-    </div>
-  </section>
-)
+export const Products = () => {
+  const [searchParams] = useSearchParams();
+  const { products, setCategory } = useProducts();
+  const category = searchParams.get('category');
+
+  useEffect(() => {
+    setCategory(category);
+  }, [category, setCategory]);
+
+  return (
+    <section className="products">
+      <div className="container">
+        <h1 className="products__title">Чай</h1>
+        <ul className="products__list">
+          {products.map((item) => (<Product key={item.id} data={item}/>))}
+        </ul>
+      </div>
+    </section>
+  )
+};
