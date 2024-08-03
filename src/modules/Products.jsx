c3d7bd3210603d8bca3d5e81dbf5e6eae284d190
Julia Kuznetsova <grace_foto@mail.ru>
2024-08-03 22:32:51 +0300

    add skeleton load

--- src/modules/Products.jsx
+++ src/modules/Products.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Product } from "./Product";
 import { useProducts } from "../context/ProductContext";
 import { useSearchParams } from "react-router-dom";
+import { SkeletonLoader } from "./SkeletonLoader";
 
 export const Products = () => {
   const [searchParams] = useSearchParams();
@@ -17,7 +18,10 @@ export const Products = () => {
       <div className="container">
         <h1 className="products__title">Чай</h1>
         <ul className="products__list">
-          {products.map((item) => (<Product key={item.id} data={item}/>))}
+          {products.length ? (
+            products.map((item) => <Product key={item.id} data={item} />)
+          ) : (<SkeletonLoader />)
+          }
         </ul>
       </div>
     </section>
