3c4c1c38645fbcc4cb9f7ab1429c2dae2881bb20
Julia Kuznetsova <grace_foto@mail.ru>
2024-08-03 18:54:00 +0300

    add modal for product card

--- src/modules/Product.jsx
+++ src/modules/Product.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+import { API_URL } from "../const"
+import { ProductModal } from "./ProductModal";
+
 export const Product = ({ data }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = (event) => {
+    event.preventDefault();
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+;
   return (
     <li className="products__item">
-      <article className="product">
-        <img className="product__image" src={data.image} alt={data.title} />
-        <div className="product__content">
-          <h3 className="product__title">{data.title}</h3>
-          <p className="product__price">{data.price}&nbsp;₽</p>
-        </div>
-      </article>
+      <a className="product__link" href="#" onClick={openModal} aria-label={`Открыть карточку с информацией о ${data.title}`}>
+        <article className="product">
+          <img className="product__image" src={`${API_URL}${data.img}`} alt={data.title} />
+          <div className="product__content">
+            <h3 className="product__title">{data.title}</h3>
+            <p className="product__price">{data.price}&nbsp;₽</p>
+          </div>
+        </article>
+      </a>
+      <ProductModal isOpen={modalIsOpen} onRequestClose={closeModal} data={data} />
     </li>
   )
 }
