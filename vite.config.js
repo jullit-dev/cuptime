762a8b26c64c193d712ca476d59d0b14fafd04fb
Julia Kuznetsova <grace_foto@mail.ru>
2024-10-04 23:34:16 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cuptime',
-  root: './src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-  },
   plugins: [react()],
 })
